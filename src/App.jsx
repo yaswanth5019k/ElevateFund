dc8537506d2c7e55433798014958a52373a51426
yaswanth5019k <yaswanthkalluri2005@gmail.com>
2025-02-08 04:12:27 +0000

    in merging conflicts not cleared

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/Navbar';
@@ -9,32 +7,15 @@ import Navbar from './components/Navbar';
 
 function App() {
     return (
-        <Router>
+        <>
             <Navbar />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/about" component={About} />
                 {/* Add more routes as needed */}
             </Switch>
-        </Router>
+        </>
     );
-=======
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import LandingPage from './components/LandingPage/LandingPage';
-import LoginPage from './components/auth/LoginPage';
-
-function App() {
-  return (
-
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-
-  );
->>>>>>> main
 }
 
 export default App;
