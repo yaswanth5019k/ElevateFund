fb175473f64622bc7d718518caf7481075afa9f6
yaswanth5019k <yaswanthkalluri2005@gmail.com>
2025-02-15 02:55:35 +0000

    added image in login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -40,23 +40,19 @@ function Login() {
     }
   };
 
-
-
   return (
     <div className="login-container">
       {/* Left Section - Illustration */}
       <div className="login-illustration">
         <div className="illustration-wrapper">
-          <div className="laptop-icon">
-            <svg viewBox="0 0 24 24" className="user-avatar">
-              <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-            </svg>
-          </div>
-          <div className="decoration-dots">
-            <span className="dot dot-1"></span>
-            <span className="dot dot-2"></span>
-            <span className="dot dot-3"></span>
-            <span className="dot dot-4"></span>
+          <img 
+            src="https://img.freepik.com/free-vector/business-team-putting-together-jigsaw-puzzle-isolated-flat-vector-illustration-cartoon-partners-working-connection-teamwork-partnership-cooperation-concept_74855-9814.jpg"
+            alt="Creative funding illustration"
+            className="login-hero-image"
+          />
+          <div className="illustration-text">
+            <h3>Welcome to Elevate Fund</h3>
+            <p>Join our community of creators and supporters</p>
           </div>
         </div>
       </div>
